9662bc61a1cced0f6428380c4b83ba6f478185fc
dnp <dnp@dotnetprofessional.com>
2014-03-02 14:29:15 -0800

    Added batch file to automatically create nuget packages and push to OneDrive

---
+++ nugetbuild.bat
@@ -0,0 +1,19 @@
+cd xpf.http
+..\.nuget\nuget.exe pack
+xcopy *.nupkg "C:\Users\Garry\SkyDrive\Public\nuget" /F /Y
+cd ..
+
+cd xpf.FileSystem
+..\.nuget\nuget.exe pack
+xcopy *.nupkg "C:\Users\Garry\SkyDrive\Public\nuget" /F /Y
+cd ..
+
+cd xpf.IO
+..\.nuget\nuget.exe pack
+xcopy *.nupkg "C:\Users\Garry\SkyDrive\Public\nuget" /F /Y
+cd ..
+
+cd xpf.IO.Script
+..\.nuget\nuget.exe pack
+xcopy *.nupkg "C:\Users\Garry\SkyDrive\Public\nuget" /F /Y
+cd ..
