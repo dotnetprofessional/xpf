3efbef2c76a3090699dfe5ab71cc33a8bfa9e800
dnp <dnp@dotnetprofessional.com>
2014-03-04 21:35:04 -0800

    Adding support for DataContract Serializers for WCF users.

---
+++ xpf.Http/HttpJsonDataContractResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xpf.Http
+{
+    public class HttpXmlDataContractResponse : HttpXmlRequest
+    {
+        public override string Serialize<T>(T data)
+        {
+            string xml = "";
+            using (var ms = new MemoryStream())
+            {
+                var xser = new DataContractSerializer(typeof(T));
+                xser.WriteObject(ms, data);
+                ms.Position = 0;
+                using (var s = new StreamReader(ms))
+                {
+                    xml = s.ReadToEnd();
+                }
+            }
+            return xml;
+        }
+    }
+}
---
+++ xpf.Http/HttpXmlDataContractResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xpf.Http
+{
+    public class HttpJsonDataContractResponse : HttpJsonRequest
+    {
+        public override string Serialize<T>(T data)
+        {
+            string xml = "";
+            using (var ms = new MemoryStream())
+            {
+                var xser = new DataContractJsonSerializer(typeof(T));
+                xser.WriteObject(ms, data);
+                ms.Position = 0;
+                using (var s = new StreamReader(ms))
+                {
+                    xml = s.ReadToEnd();
+                }
+            }
+            return xml;
+        }
+    }
+}
